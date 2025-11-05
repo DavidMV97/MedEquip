7d8a73177340fd6c3b5e3c52e3514b3acc32f6ab
Germán David <manchov12@gmail.com>
2025-11-05 11:40:30 -0500

    Add path dashboard

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+    <main>
+      <div>
+        <Outlet />
+      </div>
+    </main>
+  )
+}
+
--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout";
 import Auth from "./views/Auth";
+import Dashboard from "./views/Dashboard";
+import Layout from "./layouts/Layout";
 
 const router = createBrowserRouter([
+     {
+        path: "/",
+        element: <Layout />,
+        children: [
+            { index: true, element: <Dashboard /> },
+        ]
+    },
+    
     {
         path: "/auth",
         element: <AuthLayout />,
